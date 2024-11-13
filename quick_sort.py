b2fb64f8badb7556ab6366cafce80b36a44488cb
unknown <almaidanickee16@gmail.com>
2024-11-13 18:29:32 +0800

    remove the html and new file

---
+++ quick_sort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [ x for x in arr if x < pivot]
+    middle = [ x for x in arr if x == pivot]
+    right = [ x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [100, -1, 388, 483, 8, 10, 90, 7427]
+print(quick_sort(arr))
+
+
